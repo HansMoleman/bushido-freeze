a6525eb2c23e8a6807b44d141fa12f9a9a3012ef
Hans Moleman <aceind13@aol.com>
2023-07-02 04:51:12 -0230

    Added ability to read in and display token placed in a text file.

--- src/ghpat.c
+++ src/ghpat.c
@@ -13,10 +13,27 @@
  **/
 
 #include<stdio.h>
+#include<unistd.h>
+
 
 
 int main(int argc, char* argv[]){
-	printf("Hello, World!\n");
+	int   tkn_size = 40;
+	char  value[(tkn_size + 1)];
+	char* fpath = "data.txt";
+
+	if (access(fpath, F_OK) == 0){
+		// read in existing token
+		FILE* file_ptr;
+		file_ptr = fopen(fpath, "r");
+		fgets(value, (tkn_size + 1), (FILE*) file_ptr);
+
+		printf("%s\n", value);
+		fclose(file_ptr);
+	}
+	else {
+		printf("Error: no token to access.\n");
+	}
 
 	return 0;
 }
