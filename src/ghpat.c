63eb805e4e14e5d546245be3d2132244f5be9729
Hans Moleman <aceind13@aol.com>
2023-07-02 02:36:14 -0230

    Created src directory to hold system source code, then created ghpat.c and added basic comment header and hello world code.

---
+++ src/ghpat.c
@@ -0,0 +1,23 @@
+
+/** ghpat.c  ***
+ *
+ * CMPL:  gcc ghpat.c -Wall -o ghpat
+ * EXEC:  ./ghpat
+ * DEPN:  none
+ *
+ * (description)
+ *
+ * ---
+ *  ver-0.0.0  |  (c) 07.02.2023 (GPL-3)
+ * ---
+ **/
+
+#include<stdio.h>
+
+
+int main(int argc, char* argv[]){
+	printf("Hello, World!\n");
+
+	return 0;
+}
+
