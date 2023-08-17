4fa1b8fd3541d14fde25fd14e2ab9f1c7edb4dcf
Hans Moleman <aceind13@aol.com>
2023-08-16 22:36:28 -0230

    Overwrote ghpat contents with complete main function implemented in control.c file.

--- src/ghpat.c
+++ src/ghpat.c
@@ -12,29 +12,59 @@
  * ---
  **/
 
-#include<stdio.h>
-#include<unistd.h>
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 
 int main(int argc, char* argv[]){
-	int   tkn_size = 40;
-	char  value[(tkn_size + 1)];
-	char* fpath = "data.txt";
 
-	if (access(fpath, F_OK) == 0){
-		// read in existing token
-		FILE* file_ptr;
-		file_ptr = fopen(fpath, "r");
-		fgets(value, (tkn_size + 1), (FILE*) file_ptr);
-
-		printf("%s\n", value);
-		fclose(file_ptr);
+	if(argc == 2){
+		// only command given
+		char command[sizeof(argv[1])];
+		strcpy(command, argv[1]);
+		
+		if(strcmp(command, "new")){
+			// create new token
+			printf("<< create new token >>\n");
+		}
+		else if(strcmp(command, "view")){
+			// view target token
+			printf("<< view target token >>\n");
+		}
+		else{
+			printf("ERROR: the <'%s'> command given is unrecognized, please try again.\n", command);
+		}
 	}
-	else {
-		printf("Error: no token to access.\n");
-	}
+	else if(argc > 2){
+		// command and arg(s) given
+		char command[sizeof(argv[1])];
+		strcpy(command, argv[1]);
 
-	return 0;
+		int argl = 0;
+		for(int i = 2; i < argc; i++){
+			argl += (sizeof(argv[i]) * 2);
+		}
+		char args[argl];
+		for(int i = 2; i < argc; i++){
+			strcat(args, argv[i]);
+		}
+
+		if(strcmp(command, "new")){
+			// create new token
+			printf("<< create new token +params >>\n");
+		}
+		else if(strcmp(command, "view")){
+			// view target token
+			printf("<< view target token +params >>\n");
+		}
+		else{
+			printf("ERROR: the <'%s'> command is unrecognized, please try again.\n", command);
+		}
+	}
+	else{
+		// no command or arg(s) given
+		printf("ERROR: no command/arguments received, please try again.\n");
+	}
 }
-
