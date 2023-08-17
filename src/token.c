6aabd69224ebc436a4c60bf8cd04fc6fbf8d196d
Hans Moleman <aceind13@aol.com>
2023-08-16 23:55:01 -0230

    Implemented ability to write a single token value to binary file, as well as read a single token value from binary file with saveToken() and loadToken() methods.

--- src/token.c
+++ src/token.c
@@ -18,6 +18,10 @@
 #include <string.h>
 
 
+const char VAL_PATH[] = "valudata.bin";
+//const char MET_PATH = "metadata.bin";
+
+
 
 typedef struct Token {
     char title[64];
@@ -33,7 +37,21 @@ typedef struct Token {
  */
 
 Token* loadToken(){
-    printf("<< load token >>\n");
+    FILE* fptr_val = fopen(VAL_PATH, "rb");
+
+    if(fptr_val == NULL){
+        Token* null_token = (Token*) NULL;
+        return null_token;
+    }
+    else{
+        char tvalue[41];
+        fread(&tvalue, sizeof(tvalue), 1, fptr_val);
+        fclose(fptr_val);
+
+        Token* new_token = (Token*) malloc(sizeof(Token));
+        strcpy(new_token->value, tvalue);
+        return new_token;
+    }
 }
 
 
@@ -47,13 +65,41 @@ Token* newToken(char title[], char descr[], char value[], char expiry[]){
 }
 
 
-void saveToken(){
-    printf("<< save token >>\n");
+void saveToken(Token* a_token){
+    FILE* fptr_val = fopen(VAL_PATH, "wb");
+
+    if (fptr_val == NULL){
+        printf("ERROR: cannot open token value file.\n");
+    }
+    else{
+        char tvalue[41];
+        strcpy(tvalue, a_token->value);
+        fwrite(&tvalue, sizeof(tvalue), 1, fptr_val);
+        fclose(fptr_val);
+    }
 }
+
 
 void printToken(Token* a_token){
     printf("Name:    %s\n", a_token->title);
     printf("Descr:   %s\n", a_token->descr);
     printf("Value:   %s\n", a_token->value);
     printf("Expires: %s\n", a_token->expiry);
+}
+
+
+
+int main(int argc, char* argv[]){
+    //Token* test_token = newToken("", "", "ghp_this4is?a8sample_authorization!token", "");
+
+    // save
+    //saveToken(test_token);
+    //printf("Test token registered.\n");
+
+    // load
+    Token* test_token;
+    test_token = loadToken();
+    printf("%s\n", test_token->value);
+
+    return 0;
 }
