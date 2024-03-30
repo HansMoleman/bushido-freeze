034fd3730c30811dfea7d21738687d6c444ca7a6
HansMoleman <aceind13@aol.com>
2024-03-30 13:35:24 -0230

    Modified main file to contain 'charToBinary' and 'binaryToChar' methods from sample.c file. File 'ghpat' is result of compiling new main.c, and changes are tested working.

--- NewWork/src/main.c
+++ NewWork/src/main.c
@@ -1,76 +1,126 @@
 
-/** ghpat.c  ***
+/** main.c  ***
  *
- * CMPL:  gcc ghpat.c -Wall -o ghpat
+ * CMPL:  gcc main.c -Wall -o ghpat
  * EXEC:  ./ghpat
  * DEPN:  none
  *
  * (description)
  *
  * ---
- *  ver-0.0.0  |  (c) 07.02.2023 (GPL-3)
+ *  ver-0.0.0  |  (c) 03.28.2024 (GPL-3)
  * ---
  **/
 
 
-#include <stdlib.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include "token.h"
 
 
+void   charToBinary(char* destination, char a_char);
+char   binaryToChar(char* binary_rep);
+char** doLocalEncrypt();
+char** doLocalDecrypt();
+void   generateLocalKey();
+
+
+// MAIN FUNCTION
 
 int main(int argc, char* argv[]){
-
-	if(argc == 2){
-		// only command given
-		char command[sizeof(argv[1]) + 1];
-		strcpy(command, argv[1]);
-		
-		if(strcmp(command, "new") == 0){
-			// create new token
-			Token* temp = newToken(" ", "Test token.", "ghp_this4is?a8sample_authorization!token", " ");
-			printf("DESC:  %s\n", temp->descr);
-			saveToken(temp);
-			printf("New token registered.\n");
-		}
-		else if(strcmp(command, "view") == 0){
-			// view target token
-			Token* temp = loadToken();
-			printf("%s\n", temp->value);
-		}
-		else{
-			printf("ERROR: the <'%s'> command given is unrecognized, please try again.\n", command);
-		}
+	char test_key[] = "abcdefghijklmnopabcdefghijklmnop";
+	//int  key_length = sizeof(test_key) / sizeof(test_key[0]);
+	int key_length = strlen(test_key);
+	printf("key-length: %i", key_length);
+	char binary_key[(key_length * 9)];
+	
+	for(int i = 0; i < key_length; i++){
+		char bin_val[9];
+		charToBinary(bin_val, test_key[i]);
+		printf("%s\n", bin_val);
+		strcat(binary_key, bin_val);
 	}
-	else if(argc > 2){
-		// command and arg(s) given
-		char command[sizeof(argv[1])];
-		strcpy(command, argv[1]);
 
-		int argl = 0;
-		for(int i = 2; i < argc; i++){
-			argl += (sizeof(argv[i]) * 2);
-		}
-		char args[argl];
-		for(int i = 2; i < argc; i++){
-			strcat(args, argv[i]);
-		}
-
-		if(strcmp(command, "new")){
-			// create new token
-			printf("<< create new token +params >>\n");
-		}
-		else if(strcmp(command, "view")){
-			// view target token
-			printf("<< view target token +params >>\n");
-		}
-		else{
-			printf("ERROR: the <'%s'> command is unrecognized, please try again.\n", command);
-		}
-	}
-	else{
-		// no command or arg(s) given
-		printf("ERROR: no command/arguments received, please try again.\n");
-	}
+	printf("%s\n", binary_key);
+	int binkey_length = sizeof(binary_key) / sizeof(binary_key[0]);
+	int binkey_len = strlen(binary_key);
+	printf("size: %i\n", binkey_length);
+	printf("size: %i\n", binkey_len);
+	
+	return 0;
 }
+
+
+void charToBinary(char* destination, char a_char){
+	char binary_rep[9];
+    int remainders[9];
+    int quotient = a_char;
+    int remainder = 0;
+    int count = 0;
+
+    while(quotient > 0){
+        remainder = quotient % 2;
+        quotient = quotient / 2;
+        remainders[count] = remainder;
+        count++;
+    }
+
+    for(int i = (count - 1); 0 <= i; i--){
+        char digit[2];
+        sprintf(digit, "%d", remainders[i]);
+        strcat(binary_rep, digit);
+    }
+
+    int binstr_len = 0;
+    char target = binary_rep[binstr_len];
+    while(target != '\0'){
+        binstr_len++;
+        target = binary_rep[binstr_len];
+    }
+
+    //printf("%i\n", binstr_len);
+    while(binstr_len < 8){
+        char tempstr[9];
+        tempstr[0] = '0';
+        strcat(tempstr, binary_rep);
+        strcpy(binary_rep, tempstr);
+        binstr_len++;
+    }
+
+    strcpy(destination, binary_rep);
+}
+
+
+char  binaryToChar(char* binary_rep){
+	int power = 0;
+    int sum = 0;
+    for(int i = 7; 0 <= i; i--){
+        int base = binary_rep[i] - '0';
+        sum += (base * (1 << power));
+        power++;
+    }
+
+    char ascii = sum;
+    return ascii;
+}
+
+
+/*
+
+##### Main Success Scenario
+
+1. User prompts system to update the stored token value.
+2. System requests new token value from the user.
+3. User enters the new token value and submits it to the system. [Exc-1: submission is an unexpected length]
+4. System loads key data stored in local storage.
+5. System uses key data to encrypt the new token data.
+6. System writes new encrypted token data to local storage, overwriting (any) previous token data.
+7. System confirms operation has completed successfully and then exits.
+
+*/
+
+/*
+char** doLocalEncrypt() {
+}
+*/
