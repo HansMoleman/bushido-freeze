6739d15a5b0fca68d1e4914b63b829ca003da5dd
HansMoleman <aceind13@aol.com>
2024-03-30 17:35:23 -0230

    Implemented 'laodSBoxBin' method, which is tested working.

--- NewWork/src/subkey.c
+++ NewWork/src/subkey.c
@@ -15,7 +15,7 @@
 
 
 void loadPBoxBin(char* destination);
-void loadSBoxBin();
+void loadSBoxBin(char* destination);
 void makeSBoxes(char* destination, char* key);
 void makePBoxes(char* destination, char* key);
 void updateLocalCache(char* sboxes, char* pboxes);
@@ -25,15 +25,69 @@ void updateLocalCache(char* sboxes, char* pboxes);
 int main(int argc, char* argv[]){
 
 	char local_key[] = "0011111000100110011001010111100001101001010110010111000000111011011101000110111100101110011100110110000001011101001111000111110101100101001001010011111101100011001101100101111101001011001101100101000001110100001110100101010101001110011101100011100101101001";
-	char sboxes[((32 * 256 * 4) + 1)];
-	char pbox[((18 * 32) + 1)];
+	char sboxes[((32 * 256 * 4) + 1)] = "";
+	char pbox[((18 * 32) + 1)] = "";
 
-	loadPBoxBin(pbox);
-	printf("%s\n", pbox);
+	//loadPBoxBin(pbox);
+	//printf("%s\n", pbox);
+	loadSBoxBin(sboxes);
+	printf("%s\n", sboxes);
 
 	return 0;
 }
 
+
+
+void loadSBoxBin(char* destination){
+	char sbox1path[] = "Pi/bin/sbox1.bin";
+	char sbox2path[] = "Pi/bin/sbox2.bin";
+	char sbox3path[] = "Pi/bin/sbox3.bin";
+	char sbox4path[] = "Pi/bin/sbox4.bin";
+	char sbox1vals[((32 * 256) + 1)] = "";
+	char sbox2vals[((32 * 256) + 1)] = "";
+	char sbox3vals[((32 * 256) + 1)] = "";
+	char sbox4vals[((32 * 256) + 1)] = "";
+	char sboxes[((32 * 256 * 4) + 1)] = "";
+	FILE* fptr;
+
+	if((fptr = fopen(sbox1path, "r")) == NULL){
+		printf("ERROR");
+	}
+	else {
+		fscanf(fptr, "%s", sbox1vals);
+		fclose(fptr);
+	}
+
+	if((fptr = fopen(sbox2path, "r")) == NULL){
+		printf("ERROR");
+	}
+	else {
+		fscanf(fptr, "%s", sbox2vals);
+		fclose(fptr);
+	}
+
+	if((fptr = fopen(sbox3path, "r")) == NULL){
+		printf("ERROR");
+	}
+	else {
+		fscanf(fptr, "%s", sbox3vals);
+		fclose(fptr);
+	}
+
+	if((fptr = fopen(sbox4path, "r")) == NULL){
+		printf("ERROR");
+	}
+	else {
+		fscanf(fptr, "%s", sbox4vals);
+		fclose(fptr);
+	}
+
+	strcat(sboxes, sbox1vals);
+	strcat(sboxes, sbox2vals);
+	strcat(sboxes, sbox3vals);
+	strcat(sboxes, sbox4vals);
+	strcpy(destination, sboxes);
+}
 
 
 void loadPBoxBin(char* destination){
